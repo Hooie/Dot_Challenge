9e427edd329e4cf559ee5896c81dccb5cf89bebf
ParkSehyeon1009 <sehyeonbag21@naver.com>
2025-06-09 15:35:25 +0900

    FND Score and Functionalization

--- dot_challenge.c
+++ dot_challenge.c
@@ -8,38 +8,53 @@
 
 #define FPGA_DOT_DEVICE "/dev/fpga_dot"
 #define FPGA_TEXT_LCD_DEVICE "/dev/fpga_text_lcd"
+#define FND_FND_DEVICE "/dev/fpga_fnd"
 #define MAX_BUTTON 13
 #define MAX_OBSTACLES 10
 #define DOT_COLS 8
 #define DOT_ROWS 10
 
 unsigned char quit = 0;
+unsigned char Player[1][DOT_ROWS];
+int dev_sw, dev_dot, dev_fnd, dev_lcd, score;
 
-void user_signal1(int sig) { quit = 1; }
+typedef struct { int row, col, active; } Obstacle;
+Obstacle obs[MAX_OBSTACLES];
 
-typedef struct {
-    int row;
-    int col;
-    int active;
-} Obstacle;
+void user_signal(int s) { quit = 1; }
 
-Obstacle obstacles[MAX_OBSTACLES];
+int InitDevices()
+{
+    dev_sw = open("/dev/fpga_push_switch", O_RDWR);
+    dev_dot = open(FPGA_DOT_DEVICE, O_WRONLY);
+    dev_fnd = open(FND_FND_DEVICE, O_RDWR);
+    dev_lcd = open(FPGA_TEXT_LCD_DEVICE, O_WRONLY);
+    return (dev_sw < 0 || dev_dot < 0 || dev_fnd < 0 || dev_lcd < 0) ? -1 : 0;
+}
+
+void Cleanup()
+{
+    close(dev_sw);
+    close(dev_dot);
+    close(dev_fnd);
+    close(dev_lcd);
+}
 
 void InitObstacles()
 {
-    for (int i = 0; i < MAX_OBSTACLES; ++i)
-        obstacles[i].active = 0;
+    for (int i = 0; i < MAX_OBSTACLES; i++)
+        obs[i].active = 0;
 }
 
-void AddRandomObstacle()
+void AddObstacle()
 {
-    for (int i = 0; i < MAX_OBSTACLES; ++i)
+    for (int i = 0; i < MAX_OBSTACLES; i++)
     {
-        if (!obstacles[i].active)
+        if (!obs[i].active)
         {
-            obstacles[i].row = 0;
-            obstacles[i].col = rand() % DOT_COLS;
-            obstacles[i].active = 1;
+            obs[i].row = 0;
+            obs[i].col = rand() % DOT_COLS;
+            obs[i].active = 1;
             break;
         }
     }
@@ -47,102 +62,93 @@ void AddRandomObstacle()
 
 void UpdateObstacles()
 {
-    for (int i = 0; i < MAX_OBSTACLES; ++i)
-    {
-        if (obstacles[i].active)
-        {
-            obstacles[i].row++;
-            if (obstacles[i].row >= DOT_ROWS - 1)
-                obstacles[i].active = 0;
-        }
-    }
+    for (int i = 0; i < MAX_OBSTACLES; i++)
+        if (obs[i].active && ++obs[i].row >= DOT_ROWS - 1)
+            obs[i].active = 0;
 }
 
-void RenderObstacles(unsigned char Player[1][DOT_ROWS])
+void RenderObstacles()
 {
-    for (int r = 0; r < DOT_ROWS - 1; ++r)
-        Player[0][r] = 0x00;
-
-    for (int i = 0; i < MAX_OBSTACLES; ++i)
+    memset(Player[0], 0, DOT_ROWS - 1);
+    for (int i = 0; i < MAX_OBSTACLES; i++)
     {
-        if (obstacles[i].active)
+        if (obs[i].active)
         {
-            int r1 = obstacles[i].row;
+            int r1 = obs[i].row;
             int r2 = r1 + 1;
-
             if (r1 < DOT_ROWS - 1)
-                Player[0][r1] |= (1 << obstacles[i].col);
+                Player[0][r1] |= 1 << obs[i].col;
             if (r2 < DOT_ROWS - 1)
-                Player[0][r2] |= (1 << obstacles[i].col);
+                Player[0][r2] |= 1 << obs[i].col;
         }
     }
 }
 
 int GetPlayerCol(unsigned char b)
 {
-    for (int i = 0; i < DOT_COLS; ++i)
+    for (int i = 0; i < 8; i++)
         if ((b >> i) & 1)
             return i;
     return -1;
 }
 
-int CheckCollision(unsigned char playerByte)
+int CheckCollision()
 {
-    int pc = GetPlayerCol(playerByte);
-    for (int i = 0; i < MAX_OBSTACLES; ++i)
-        if (obstacles[i].active &&
-            obstacles[i].row + 1 >= DOT_ROWS - 1 &&
-            obstacles[i].col == pc)
+    int c = GetPlayerCol(Player[0][DOT_ROWS - 1]);
+    for (int i = 0; i < MAX_OBSTACLES; i++)
+        if (obs[i].active && obs[i].row + 1 >= DOT_ROWS - 1 && obs[i].col == c)
             return 1;
     return 0;
 }
 
 void FormatFND(int v, unsigned char d[4])
 {
-    for (int i = 3; i >= 0; --i) { d[i] = v % 10; v /= 10; }
+    for (int i = 3; i >= 0; i--)
+        d[i] = v % 10, v /= 10;
+    write(dev_fnd, d, 4);
+}
+
+void HandleInput()
+{
+    unsigned char sb[MAX_BUTTON];
+    read(dev_sw, sb, MAX_BUTTON);
+    if (sb[0] == 1 && (Player[0][DOT_ROWS - 1] & 0x80) == 0)
+        Player[0][DOT_ROWS - 1] <<= 1;
+    else if (sb[2] == 1 && (Player[0][DOT_ROWS - 1] & 0x01) == 0)
+        Player[0][DOT_ROWS - 1] >>= 1;
+}
+
+void GameLoop()
+{
+    InitObstacles();
+    score = 0;
+    Player[0][DOT_ROWS - 1] = 0x08;
+
+    int it = 0, gt = 0, tick = 0;
+    while (!quit)
+    {
+        usleep(10000); it += 10; gt += 10;
+        if (it >= 50) { HandleInput(); it = 0; }
+        if (gt >= 1000)
+        {
+            UpdateObstacles();
+            if (tick++ % 2 == 0) AddObstacle();
+            RenderObstacles();
+            if (CheckCollision()) { printf("Game Over\n"); break; }
+            unsigned char fd[4] = { 0 };
+            FormatFND(++score, fd);
+            write(dev_dot, Player[0], DOT_ROWS);
+            gt = 0;
+        }
+    }
 }
 
 int main(void)
 {
-    signal(SIGINT, user_signal1);
+    signal(SIGINT, user_signal);
     srand(time(NULL));
-
-    int dev_sw = open("/dev/fpga_push_switch", O_RDWR);
-    int dev_dot = open(FPGA_DOT_DEVICE, O_WRONLY);
-    int dev_lcd = open(FPGA_TEXT_LCD_DEVICE, O_WRONLY);
-    if (dev_sw < 0 || dev_dot < 0 || dev_fnd < 0 || dev_lcd < 0) return -1;
-
-    unsigned char Player[1][DOT_ROWS] = { {0} };
-    Player[0][DOT_ROWS - 1] = 0x08;  // start at center
-    unsigned char swbuff[MAX_BUTTON];
-
-    InitObstacles();
-    int tick = 0, score = 0;
-    while (!quit)
-    {
-        usleep(1000000);
-        UpdateObstacles();
-        if (tick % 2 == 0) AddRandomObstacle();
-        RenderObstacles(Player);
-
-        read(dev_sw, swbuff, sizeof(swbuff));
-        if (swbuff[0] == 1 && (Player[0][DOT_ROWS - 1] & 0x80) == 0)
-            Player[0][DOT_ROWS - 1] <<= 1;
-        else if (swbuff[2] == 1 && (Player[0][DOT_ROWS - 1] & 0x01) == 0)
-            Player[0][DOT_ROWS - 1] >>= 1;
-
-        if (CheckCollision(Player[0][DOT_ROWS - 1]))
-        {
-            printf("Game Over\n");
-            break;
-        }
-
-        write(dev_dot, Player[0], DOT_ROWS);
-        tick++;
-    }
-
-    close(dev_sw);
-    close(dev_dot);
-    close(dev_lcd);
+    if (InitDevices() < 0) return -1;
+    GameLoop();
+    Cleanup();
     return 0;
 }
